d0786561ee826612a993b74d1e802c9548c698eb
Chris Delaney <cadelaney3@gmail.com>
2019-04-13 16:39:15 -0700

    new websocket

--- server/src/app/main.go
+++ server/src/app/main.go
@@ -43,17 +43,12 @@ func reader(conn *websocket.Conn) {
 
 // define our WebSocket endpoint
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	// upgrade this connection to a WebSocket
-	// connection
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(w, "%+V\n", err)
 	}
-	// listen indefinitely for new messages coming
-	// through on our WebSocket connection
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setupRoutes() {
