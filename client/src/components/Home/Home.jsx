be13f1c4d51dc6b1f1162d2047db5060dbc0dca0
Chris Delaney <cadelaney3@gmail.com>
2019-06-12 18:15:37 -0600

    styling

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -6,14 +6,16 @@ import Grid from '@material-ui/core/Grid';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
+import Octicon, {MarkGithub} from '@primer/octicons-react';
+import Avatar from '@material-ui/core/Avatar';
 import mugshot from '../../images/mugshot.jpg';
+import linkedinLogo from '../../images/In-Blue-26@2x.png';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-    paddingTop: theme.spacing.unit * 8,
-    //paddingLeft: theme.spacing.unit * 4,
+    paddingTop: theme.spacing.unit * 4,
   },
   image: {
       width: 256,
@@ -37,17 +39,41 @@ const styles = theme => ({
   inline: {
     display: 'inline',
   },
+  link: {
+    textDecoration: 'none',
+    color: '#ffd600',
+  },
+  avatar: {
+      width: "25px",
+      height: "25px",
+      marginTop: '5px',
+  },
+  typography: {
+      paddingTop: "8px",
+      paddingBottom: "8px",
+      color: "#ffd600",
+      fontFamily: 'Roboto',
+  },
+  body: {
+      fontFamily: 'sans-serif',
+      color: '#fff176',
+  },
+  octicon: {
+      height: '25px',
+      width: '25px',
+      paddingX: '3px',
+      verticalAlign: 'center',
+      color: 'white',
+  },
 });
 
 class Home extends Component {
     render() {
       const { classes } = this.props;   
-      console.log(this.props);
-      console.log(this.props.items.body);
       const facts = this.props.items.facts.map(fact =>
         <ListItem key={fact}>
             <Paper className={classes.contentPaper}>
-                <Typography variant="body2" align="left">
+                <Typography className={classes.body} variant="body2" align="left">
                     {fact}
                 </Typography>
             </Paper>
@@ -64,28 +90,38 @@ class Home extends Component {
                         </Grid>
                         <Grid item>
                             <Paper className={classes.firstColPaper}>
-                                <Typography variant="body2" align="left">
-                                    github.com/cadelaney3
-                                </Typography>
-                                <Typography variant="body2" align="left">
-                                    linked.com/in/cadelaney3
-                                </Typography>
+                                    <Link href="https://github.com/cadelaney3/" className={classes.link} target="_blank" rel="noopener" >
+                                        <Octicon className={classes.octicon} icon={MarkGithub} ariaLabel="GitHub" noWrap />
+                                        {"https://github.com/cadelaney3"}
+                                    </Link>
+                                    <Link href="https://linked.com/in/cadelaney3/" className={classes.link} target="_blank" rel="noopener">
+                                    <Grid container justify="center" alignItems="center">
+                                        <Avatar className={classes.avatar} alt="in" src={linkedinLogo} inline="true" />
+                                        {"https://linkedin.com/in/cadelaney3"}
+                                    </Grid>
+                                    </Link>
                             </Paper>
                         </Grid>
                     </Grid>
                 </Grid>
                 <Grid item xs={12} sm container>
                     <Grid item xs container direction="column" spacing={8}>
+                        <Typography className={classes.typography} variant="h2" align="left">
+                            Bio
+                        </Typography>
                         <List>
                             <ListItem key={this.props.items.body}>
-                                <Paper className={classes.contentPaper}>
-                                    <Typography variant="body1" align="left">
-                                        {this.props.items.body}
-                                    </Typography>
-                                </Paper>
+                                <Typography className={classes.body} variant="body1" align="left">
+                                    {this.props.items.body}
+                                </Typography>
                             </ListItem>
-                            {facts}
                         </List>
+                        <Typography className={classes.typography} variant="h2" align="left">
+                            Fun Facts
+                        </Typography>
+                        <List>
+                            {facts}
+                        </List>                      
                     </Grid>
                 </Grid>
             </Grid>
