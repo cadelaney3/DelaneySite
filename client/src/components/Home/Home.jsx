8330ae0009b288557051eecd4f34f23828698670
Chris Delaney <cadelaney3@gmail.com>
2019-07-02 22:19:54 -0600

    convert from class to hook

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { withStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/List';
 import Grid from '@material-ui/core/Grid';
@@ -15,7 +15,7 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import Resume from '../../files/ChrisDelaney_Resume.pdf';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     paddingTop: theme.spacing(4),
@@ -79,83 +79,116 @@ const styles = theme => ({
       right: 20,
       left: 'auto',
   }
-});
+}));
 
-class Home extends Component {
-    constructor(props) {
-        super(props);
+export default function About(props) {
+    const classes = useStyles();
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
 
-        this.state = {
-            loggedIn: sessionStorage.getItem("loggedIn"),
+    const getResults = () => {
+        //fetch("http://localhost:8080/home")
+        fetch("http://172.17.21.104:8080/home")
+        .then(res => res.json())
+        .then(result => {
+            setItems(result);
+            setIsLoaded(true);
+        })
+        .catch(error => {
+            setIsLoaded(true);
+            setError(error);
+        })
+    };
+
+    useEffect(() => {
+        getResults();
+    }, []);
+
+    const [facts, setFacts] = useState([]);
+
+    useEffect(() => {
+        if (items.facts) {
+            setFacts(items.facts.map(fact =>
+                <ListItem key={fact}>
+                    <Paper className={classes.contentPaper}>
+                        <Typography className={classes.body} variant="body2" align="left">
+                            {fact}
+                        </Typography>
+                    </Paper>
+                </ListItem>
+            ));
         }
-    }
-    
-    render() {
-      const { classes } = this.props;   
-      const facts = this.props.items.facts.map(fact =>
-        <ListItem key={fact}>
-            <Paper className={classes.contentPaper}>
-                <Typography className={classes.body} variant="body2" align="left">
-                    {fact}
-                </Typography>
-            </Paper>
-        </ListItem>
-      );
-      return (
-        <Grid container className={classes.root} spacing={8}>
-            <Grid item xs={3}>
-                <Grid container direction="column" spacing={8}>
-                    <Grid item>
-                        <ButtonBase className={classes.image}>
-                            <img className={classes.img} alt="Chris" src={mugshot} />
-                        </ButtonBase>
-                    </Grid>
-                    <Grid item>
-                        <Paper className={classes.firstColPaper} align='left'>
-                                <Link href="https://github.com/cadelaney3/" className={classes.link} target="_blank" rel="noopener" >
-                                    <Octicon className={classes.octicon} icon={MarkGithub} ariaLabel="GitHub" noWrap />
-                                    {"https://github.com/cadelaney3"}
-                                </Link>
-                                <Link href="https://linked.com/in/cadelaney3/" className={classes.link} target="_blank" rel="noopener">
-                                <Grid container alignItems="center">
-                                    <Avatar className={classes.avatar} alt="in" src={linkedinLogo} inline="true" />
-                                    {"https://linkedin.com/in/cadelaney3"}
-                                </Grid>
-                                </Link>
-                                <Link href={Resume} className={classes.link} target="_blank" rel="noopener">
-                                    <Octicon className={classes.octicon} icon={File} ariaLabel="File" noWrap />
-                                    {"My Resume"}
-                                </Link>
-                        </Paper>
+    }, [items]);
+
+    const [about, setAbout] = useState(null);
+
+    useEffect(() => {
+        setAbout(
+            <Grid container className={classes.root} spacing={8}>
+                <Grid item xs={3}>
+                    <Grid container direction="column" spacing={8}>
+                        <Grid item>
+                            <ButtonBase className={classes.image}>
+                                <img className={classes.img} alt="Chris" src={mugshot} />
+                            </ButtonBase>
+                        </Grid>
+                        <Grid item>
+                            <Paper className={classes.firstColPaper} align='left'>
+                                    <Link href="https://github.com/cadelaney3/" className={classes.link} target="_blank" rel="noopener" >
+                                        <Octicon className={classes.octicon} icon={MarkGithub} ariaLabel="GitHub" noWrap />
+                                        {"https://github.com/cadelaney3"}
+                                    </Link>
+                                    <Link href="https://linked.com/in/cadelaney3/" className={classes.link} target="_blank" rel="noopener">
+                                    <Grid container alignItems="center">
+                                        <Avatar className={classes.avatar} alt="in" src={linkedinLogo} inline="true" />
+                                        {"https://linkedin.com/in/cadelaney3"}
+                                    </Grid>
+                                    </Link>
+                                    <Link href={Resume} className={classes.link} target="_blank" rel="noopener">
+                                        <Octicon className={classes.octicon} icon={File} ariaLabel="File" noWrap />
+                                        {"My Resume"}
+                                    </Link>
+                            </Paper>
+                        </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
-            <Grid item xs={12} sm container>
-                <Grid item xs container direction="column" spacing={8}>
-                    <Typography className={classes.typography} variant="h2" align="left">
-                        Bio
-                    </Typography>
-                    <List>
-                        <ListItem key={this.props.items.body}>
-                            <Typography className={classes.body} variant="body1" align="left">
-                                {this.props.items.body}
-                            </Typography>
-                        </ListItem>
-                    </List>
-                    <Typography className={classes.typography} variant="h2" align="left">
-                        Fun Facts
-                    </Typography>
-                    <List>
-                        {facts}
-                    </List>                      
+                <Grid item xs={12} sm container>
+                    <Grid item xs container direction="column" spacing={8}>
+                        <Typography className={classes.typography} variant="h2" align="left">
+                            Bio
+                        </Typography>
+                        <List>
+                            <ListItem key={items.body}>
+                                <Typography className={classes.body} variant="body1" align="left">
+                                    {items.body}
+                                </Typography>
+                            </ListItem>
+                        </List>
+                        <Typography className={classes.typography} variant="h2" align="left">
+                            Fun Facts
+                        </Typography>
+                        <List>
+                            {facts}
+                        </List>                      
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Fab aria-label="Add" className={classes.fab}>
-                <AddIcon />
-            </Fab>
-        </Grid>
-      );
+                {(sessionStorage.getItem("loggedIn") === "false") &&
+                    <Fab aria-label="Add" className={classes.fab}>
+                        <AddIcon />
+                    </Fab>
+                }
+            </Grid>  
+        );
+    }, [facts])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            about            
+        );
     }
 }
-  
-export default withStyles(styles)(Home);
